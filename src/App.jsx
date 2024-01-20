bc8c35e003ac1d1099a12f4dc7a399c5226da3be
unknown <dipeshhaldar787@gmail.com>
2024-01-20 14:24:02 +0530

    harshal

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/CoreConcepts";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <CoreConcepts />
+      </main>
+    </div>
+  );
+}
+
+export default App;
